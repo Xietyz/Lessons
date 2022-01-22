4b504adfa6d0a2d3724ecb283e213a9a48a5f570
Sean <shoang1550183@gmail.com>
2022-01-22 02:23:49 +0000

    Add project for classes

---
+++ ClassesAndInheritance/ClassesAndInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesAndInheritance
+{
+    class Program
+    {
+        // let person = {
+        //     name1: "Sean",
+        //     name2: "Hoang",
+        //     hairColour: "Black",
+        //     age: 24
+        // }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
